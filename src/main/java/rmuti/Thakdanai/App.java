c487cbc0590b611ee7c1a1734113fc967b38e4ba
Thakdanai <l3oomkoong2@gmail.com>
2017-04-11 17:58:12 +0700

    test add data

---
+++ src/main/java/rmuti/Thakdanai/App.java
@@ -0,0 +1,16 @@
+package rmuti.Thakdanai;
+
+import java.util.ArrayList;
+
+public class App {
+	public static void main(String[] args) {
+		ArrayList<Object> lst = new ArrayList();
+		lst.add(9);
+		lst.add(6);
+		lst.add(4);
+		lst.add(5);
+		lst.add(1);
+
+		System.out.println(lst.toString());
+	}
+}
